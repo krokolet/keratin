f8bca4eee5fac68754a1f02af426283f294d9ce9
krokolet <krokolet@gamil.com>
2021-08-28 18:57:20 +0700

    Social box добавлен. Попытка победить 100vh на мобильных

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import Header from './components/Header/Header';
 import Slider from './components/Slider/Slider';
 import PriceBox from './components/PriceBox/PriceBox';
 import Record from './components/Record/Record';
+import SocialBox from './components/SocialBox/SocialBox';
 
 function App() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    window.addEventListener('resize', () => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
     return (
         <div className="wrapper">
             <Header />
             <Slider />
             <PriceBox />
             <Record />
-            <div className="social" />
+            <SocialBox />
         </div>
     );
 }
---
+++ src/components/SocialBox/SocialBox.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './socialBox.scss';
+
+const SocialBox = () => {
+    return (
+        <div className="socialBox">
+            <a className="icon icon--Instagram" />
+            <a className="icon icon--VK"></a>
+            <a className="icon icon--OK"></a>
+        </div>
+    );
+};
+
+export default SocialBox;
