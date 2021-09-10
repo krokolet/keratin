e590676649dbce4db20b9be9de58d2cc18ad0e01
krokolet <krokolet@gmail.com>
2021-09-10 10:58:17 +0700

    unusade var del

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
+import React, { useState } from 'react';
+
 import './App.scss';
 import 'antd/dist/antd.css';
 
-import './components/Header/Header';
 import Header from './components/Header/Header';
 import Slider from './components/Slider/Slider';
 import PriceBox from './components/PriceBox/PriceBox';
 import RecordBtn from './components/RecordBtn/RecordBtn';
 import RecordBox from './components/RecordBox/RecordBox';
-
-import React, { useState } from 'react';
-import { Modal, Button, Row, Col } from 'antd';
 
 function App() {
 	let vh = window.innerHeight * 0.01;
@@ -19,17 +17,18 @@ function App() {
 		document.documentElement.style.setProperty('--vh', `${vh}px`);
 	});
 
-		const [isModalVisible, setIsModalVisible] = useState(false);
+	const [isModalVisible, setIsModalVisible] = useState(false);
 	return (
 		<div className="wrapper">
 			<Header />
 			<Slider />
 			<PriceBox />
-			{/* <button className="recordBtn">Записаться</button> */}
-			<RecordBtn setIsModalVisible={setIsModalVisible }/>
-			<RecordBox isModalVisible={isModalVisible} setIsModalVisible={ setIsModalVisible}/>
+			<RecordBtn setIsModalVisible={setIsModalVisible} />
+			<RecordBox
+				isModalVisible={isModalVisible}
+				setIsModalVisible={setIsModalVisible}
+			/>
 		</div>
-
 	);
 }
 
