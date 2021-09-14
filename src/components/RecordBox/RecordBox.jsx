8767ee03fee4eddf85912a099a00a6f00072eee9
krokolet <krokolet@gmail.com>
2021-09-14 17:24:50 +0700

    social link

--- src/components/RecordBox/RecordBox.jsx
+++ src/components/RecordBox/RecordBox.jsx
@@ -1,59 +1,65 @@
 import React from 'react';
 import { Modal } from 'antd';
-import { telephone, telegramm } from '../../data/masterContacts';
+import {
+	telephone,
+	telegramm,
+	odnoklassniki,
+	vkontakte,
+	instagram,
+} from '../../data/masterContacts';
 import './RecordBox.scss';
 
 const RecordBox = ({ isVisible, setIsVisible }) => {
     return (
-        <Modal
-            title="Всегда на связи !"
-            visible={isVisible}
-            onCancel={() => setIsVisible(false)}
-            footer={null}
-            centered
-        >
-            <nav className="contactList">
-                <p className="phone messengerBtn">
-                    <a
-                        className="phone__number contact-text"
-                        href={`tel:${telephone}`}
-                    >
-                        {telephone}
-                    </a>
-                </p>
-                <p className="whatsapp messengerBtn">
-                    <a
-                        className="whatsapp__number  contact-text"
-                        href={`https://wa.me/${telephone}?text=${encodeURI(
-                            'Здравствуйте, я хочу к вам записаться !'
-                        )}`}
-                    >
-                        <span className="whatsapp__text">Whatsapp</span>
-                    </a>
-                </p>
-                <p className="telegram messengerBtn">
-                    <a
-                        className="telegram__number  contact-text"
-                        href={`tg://resolve?domain=${telegramm}`}
-                    >
-                        <span className="telegram__text">Telegram</span>
-                    </a>
-                </p>
-                <div>- или -</div>
-                <section>
-                    <a href="https://m.vk.com/id376777946" >
-                        <span className="odnoklassniki social"></span>
-                    </a>
-                    <a href="https://m.vk.com/id376777946" >
-                        <span className="vkontakte social"></span>
-                    </a>
-                    <a href="https://m.vk.com/id376777946" >
+		<Modal
+			title="Всегда на связи !"
+			visible={isVisible}
+			onCancel={() => setIsVisible(false)}
+			footer={null}
+			centered
+		>
+			<nav className="contactList">
+				<p className="phone messengerBtn">
+					<a
+						className="phone__number contact-text"
+						href={`tel:${telephone}`}
+					>
+						{telephone}
+					</a>
+				</p>
+				<p className="whatsapp messengerBtn">
+					<a
+						className="whatsapp__number  contact-text"
+						href={`https://wa.me/${telephone}?text=${encodeURI(
+							'Здравствуйте, я хочу к вам записаться !'
+						)}`}
+					>
+						<span className="whatsapp__text">Whatsapp</span>
+					</a>
+				</p>
+				<p className="telegram messengerBtn">
+					<a
+						className="telegram__number  contact-text"
+						href={`tg://resolve?domain=${telegramm}`}
+					>
+						<span className="telegram__text">Telegram</span>
+					</a>
+				</p>
+				<div>- или -</div>
+				<section>
+					<a href={`https://${odnoklassniki}`}>
+						<span className="odnoklassniki social"></span>
+					</a>
+					<a href={`https://${vkontakte}`}>
+						<span className="vkontakte social"></span>
+					</a>
+                    <a href={`https://${instagram}`}>
                         <span className="instagram social"></span>
-                    </a>
-                </section>
-            </nav>
-        </Modal>
-    );
+					</a>
+				</section>
+			</nav>
+		</Modal>
+	);
 };
 
 export default RecordBox;
--- src/data/masterContacts.js
+++ src/data/masterContacts.js
@@ -1,2 +1,5 @@
 export const telephone = '+79505912136';
 export const telegramm = 'sudmiv';
+export const odnoklassniki = 'ok.ru/profile/511733740649';
+export const vkontakte = 'vk.com/id376777946';
+export const instagram = 'instagram.com/keratin_nano_as/?hl=ru';
